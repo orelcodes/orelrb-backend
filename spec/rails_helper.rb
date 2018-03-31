250e2c90c366dd5b4d9412cc46edb9431dbdc28b
Kirill Karpinovsky <karpinovsky@gmail.com>
2018-03-31 18:47:20 +0300

    Require supports

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require_relative 'support/shoulda_matchers'
+Dir[Rails.root.join('spec/support/*.rb')].each { |f| require f }
+require 'faker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
